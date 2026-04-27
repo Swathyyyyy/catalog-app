6b335a2bbef8a03d5380065e0bbb19d74f7575f9
Swathi M <muruganswathi57@gmail.com>
2026-04-27 21:02:04 +0530

    Fixed some UI  and completed assignment

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import data from "../data/data.json";
+
 
 function Detail() {
   const { id } = useParams();
   const item = data[id];
 
+  const navigate = useNavigate();
+
   return (
     <div className="container">
+      <div className="top-bar">
+        <button onClick={() => navigate("/")}>⬅ Back</button>
+      </div>
+
       <h1>{item.itemname}</h1>
-      <img src={item.image} alt={item.itemname} />
+
+      <img  className="detail-img" src={item.image} alt={item.itemname} />
 
       <h3>Category: {item.category}</h3>
 
       {item.itemprops.map((prop, index) => (
         <p key={index}>
-          {prop.label}: {prop.value}
+          <strong>{prop.label}:</strong> {prop.value}
         </p>
       ))}
     </div>
